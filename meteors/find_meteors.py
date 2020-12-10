e4c5eef472c5d9ea3d5759534f8c7818368a02fe
Amtul Malik <75287754+zkmalik01@users.noreply.github.com>
2020-12-09 21:34:38 -0500

    Don't run scrip code when imported

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -18,18 +18,18 @@ def get_dist(meteor):
     return meteor.get('distance', math.inf)
 
 if __name__ == '__main__':
-    my_loc = (29.424122, -98.493628)
+        my_loc = (29.424122, -98.493628)
 
-    meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
-    meteor_data = meteor_resp.json()
+        meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
+        meteor_data = meteor_resp.json()
 
-    for meteor in meteor_data:
-        if not ('reclat' in meteor and 'reclong' in meteor): continue
-        meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                       float(meteor['reclong']),
-                                       my_loc[0],
-                                       my_loc[1])
+        for meteor in meteor_data:
+            if not ('reclat' in meteor and 'reclong' in meteor): continue
+            meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                           float(meteor['reclong']),
+                                           my_loc[0],
+                                           my_loc[1])
 
-    meteor_data.sort(key=get_dist)
+        meteor_data.sort(key=get_dist)
 
-    print(meteor_data[0:10])
+        print(meteor_data[0:10])
